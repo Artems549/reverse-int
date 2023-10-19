95089136838f61edf0f830412cd4cc7b81ac396a
Artem <sfmillingg@gmail.com>
2023-10-19 12:24:28 +0400

    feat; add new function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 module.exports = function reverse (n) {
-    let array = Array.from(String(n), Number);
+    let array = Array.from(String(n));
+    if(array[0] === '-') {
+        array.splice(0, 1);
+    }
+    if(array[array.length-1] === '0') {
+        array.splice(array.length - 1, 1)
+    }
     let result = array.reverse().join("")
     return Number(result)
 }
